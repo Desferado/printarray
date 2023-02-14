51c46d9fb15eb99cedb08431ae470794bb4f2740
Denis_vrn <denisfedorov50@gmail.com>
2023-02-14 23:36:05 +0300

    Вывод массива на консоль в обратной последовательности. Готово/ Добавил новое решение.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int [] array = {5,4,3,2,1,0};
@@ -10,5 +12,11 @@ public class Main {
                 System.out.print(array[i] + "]");
             }
         }
+        System.out.println();
+        int [] arrayNew = new int [6];
+        for (int i = 0; i < arrayNew.length; i++){
+            arrayNew[i] = array[array.length-1-i];
+        }
+        System.out.println(Arrays.toString(arrayNew));
     }
 }
